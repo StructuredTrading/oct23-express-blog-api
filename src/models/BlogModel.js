56f7bcb5b46dff100104f05bdedfe554022553bc
StructuredTrading <samowen1992@hotmail.com>
2024-06-22 09:54:27 +1000

    feat: subdocument and document references declared

--- src/models/BlogModel.js
+++ src/models/BlogModel.js
@@ -2,59 +2,70 @@
 - Title
 - Content
 - User (posted by)
-- Created date
 - Like 
 - Image upload 
 - Category/Tags/keywords 
 - Audit history
 	- user 
 	- timestamp 
- */
+*/
 
 const mongoose = require("mongoose");
+const { commentSchema } = require("./CommentSchema.js");
 
 const blogSchema = mongoose.Schema({
-    title: {
-        type: String,
-        require: true
-    },
-    content: {
-        type: String,
-        require: true
-    },
-    author: {
-        type: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
-        required: true
-    },
-    likes: {
-        type: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
-        required: false
-    },
-    headerImage: {
-        type: String, // URL to the file/imag storage provider
-        required: false
-    },
-    tags: { // keywords defined by the blog post author
-        type: [String], // ["life", "travel", "photography"]
-        required: true
-    },
-    categories: { // post category defined by website admin/developer 
-        type: [String], // ["life", "travel", "photography"]
-        enum: ["life", "travel", "photography", "coding"],
-        required: true
-    },
-    editHistory: {
-        type: [{user: String, timestamp: Date}],
-        required: false
-    }
+	title: {
+		type: String,
+		required: true
+	},
+	content: {
+		type: String, 
+		required: true
+	},
+	author: {
+		type: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
+		required: true
+	},
+	likes: {
+		type: [{type: mongoose.Schema.Types.ObjectId, ref: "User"}], 
+		required: false
+	},
+	headerImage: {
+		type: String, // URL to the file/image storage provider 
+		required: true
+	},
+	tags: { // keywords defined by the blog post author
+		type: [String], // ["life", "travel", "photography"]
+		required: true
+	},
+	categories: { // post category defined by website admin/developer 
+		type: [String], // ["life", "travel", "photography"]
+		enum: ["life", "travel", "photography", "coding"],
+		required: true
+	},
+	editHistory: {
+		type: [{user: String, timestamp: Date}],
+		required: false
+	},
+	// This is what we would write if we do NOT use subdocuments:
+	// commentsAsObj: {
+	// 	type: [{userId: {type: mongoose.Schema.Types.ObjectId, ref: "User"}, content: {type: String}}],
+	// 	required: false
+	// },
+	// This is what we would write if we DO use subdocuments: 
+	comments: {
+		type: [commentSchema],
+		required: false
+	}
 },
 {
-    timestamps: true,
+	timestamps: true
 });
 
 
-const BlogModel = mongoose.Model("Blog", blogSchema);
+const BlogModel = mongoose.model("Blog", blogSchema);
 
-module.export = {
-    BlogModel
+module.exports = {
+	BlogModel
 }
+
